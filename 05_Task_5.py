23689cc27617dacac1e3e88844337b3b946cb4ed
Evgeny Turilov <evgenyturilov@gmail.com>
2022-05-03 18:53:13 +0300

    rewrite code using import of function from external file

--- 05_Task_5.py
+++ 05_Task_5.py
@@ -1,20 +1,16 @@
 # Задача 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
-def fibonacci(n):
-    if n == 1 or n == 2:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+import Fibonnaci_rec as f_rec         # Импорт функции расчета чисел Фибоначчи рекурсивным методом из файла Fibonacci_rec.py
 
 def list_of_negafibonacci(num):
     list_of_negafibonacci = list()
     for i in range(-num, num + 1):
         if i < 0:
-            list_of_negafibonacci.append(-fibonacci(abs(i)))
+            list_of_negafibonacci.append(-f_rec.fibonacci_rec(abs(i)))
         elif i == 0:
             list_of_negafibonacci.append(0)
         else:
-            list_of_negafibonacci.append(fibonacci(i))
+            list_of_negafibonacci.append(f_rec.fibonacci_rec(i))
     return list_of_negafibonacci
 
 
